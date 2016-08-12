eb7ea0bc8f493c8d455b07564e2718d0c5ff70cc
Ethan Law <ethanlaw@Ethans-MacBook-Pro.local>
2016-08-11 22:11:40 -0700

    add 3 more questions to political survey

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ $(document).ready(function() {
     var total = 0;
     var q1 = $("input:radio[name=question1]:checked").val();
     var q2 = $("input:radio[name=question2]:checked").val();
+    var q3 = $("input:radio[name=question3]:checked").val();
+    var q4 = $("input:radio[name=question4]:checked").val();
+    var q5 = $("input:radio[name=question5]:checked").val();
 
     if (q1 === "true"){
       total += 1;
@@ -17,14 +20,27 @@ $(document).ready(function() {
     if (q2 === "true"){
       total += 1;
     }
+    if (q3 === "true"){
+      total += 1;
+    }
+    if (q4 === "true"){
+      total += 1;
+    }
+    if (q5 === "true"){
+      total += 1;
+    }
 
     if (age && name) {
-      if (total > 1) {
-        alert("Thanks " + name + "! Your score is: " + total + " You are a liberal.")
-      } else if (total === 1) {
-        alert("Thanks " + name + "! Your score is: " + total + " You are a moderate.")
-      } else {
-        alert("Thanks " + name + "! Your score is: " + total + " You are a conservative.")
+      if (total === 5) {
+        alert("Thanks " + name + "! Your score is: " + total + "/5 Bernies. You are a communist.")
+      } else if (total === 4) {
+        alert("Thanks " + name + "! Your score is: " + total + "/5 Bernies. You are a liberal.")
+      } else if (total === 3) {
+        alert("Thanks " + name + "! Your score is: " + total + "/5 Bernies. You are a moderate.")
+      } else if (total === 2) {
+        alert("Thanks " + name + "! Your score is: " + total + "/5 Bernies. You are a conservative.")
+      } else if (total <= 1) {
+        alert("Thanks " + name + "! Your score is: " + total + "/5 Bernies. You are Ron Paul.")
       }
       $(".form-group").removeClass("has-error")
       } else {
